4cb325bac9e490d5de3d76ab5561ea298d8a925a
vbforge <vladbogdantsev@gmail.com>
2026-03-07 15:25:32 +0200

    hw7

--- java-core/hello-world/src/test/java/com/vbforge/HelloWorldTest.java
+++ java-core/hello-world/src/test/java/com/vbforge/HelloWorldTest.java
@@ -26,7 +26,7 @@ public class HelloWorldTest {
 
     @Test
     public void testMainWithArguments() {
-        assertDoesNotThrow(() -> HelloWorld.main(new String[]{"arg1", "arg2"}));
+        assertDoesNotThrow(() -> HelloWorld.main(new String[]{"arg1", "arg2", "arg3"}));
     }
 
 }
