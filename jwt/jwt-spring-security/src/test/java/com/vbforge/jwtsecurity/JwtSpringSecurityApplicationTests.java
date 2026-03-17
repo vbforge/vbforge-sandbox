1dfc2db7d9801b35d644955de9e3f2228ed18305
vbforge <vladbogdantsev@gmail.com>
2026-03-17 13:52:23 +0200

    testing commit in branch: feature/security-issues

--- jwt/jwt-spring-security/src/test/java/com/vbforge/jwtsecurity/JwtSpringSecurityApplicationTests.java
+++ jwt/jwt-spring-security/src/test/java/com/vbforge/jwtsecurity/JwtSpringSecurityApplicationTests.java
@@ -3,11 +3,20 @@ package com.vbforge.jwtsecurity;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class JwtSpringSecurityApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testSuccess() {
+        String text = "text";
+        assertEquals(4, text.length());
     }
 
 }
